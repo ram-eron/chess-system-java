04a97259e862468636a752f83846fe21bda61c67
Eron Carneiro <eron3ram.gmail.com>
2020-10-04 17:23:55 -0300

    first class, Position

---
+++ src/model/board/layer/Position.java
@@ -0,0 +1,34 @@
+package model.board.layer;
+
+public class Position {
+
+	private Integer linha;
+	private Integer coluna;
+	
+	public Position() {
+	}
+
+	public Position(Integer linha, Integer coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	public Integer getLinha() {
+		return linha;
+	}
+
+	public void setLinha(Integer linha) {
+		this.linha = linha;
+	}
+
+	public Integer getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(Integer coluna) {
+		this.coluna = coluna;
+	}
+	
+	
+	
+}
